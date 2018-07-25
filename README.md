46a2171b80d1f8e47ba135dc6caa693655375794
Anton Art <toni.artemenko2@gmail.com>
2018-07-26 00:22:04 +0300

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# article-list-react
-Created with CodeSandbox
---
+++ src/components/ArticleList/Article.js
@@ -0,0 +1,73 @@
+import React, { PureComponent } from "react";
+
+class Article extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0
+    };
+  }
+
+  /*
+        shouldComponentUpdate(nextProps, nextState) {
+            return this.state.isOpen !== nextState.isOpen
+        }
+    */
+
+  componentWillMount() {
+    console.log("---", "mounting");
+  }
+
+  /*
+        componentWillReceiveProps(nextProps) {
+            if (nextProps.defaultOpen !== this.props.defaultOpen) this.setState({
+                isOpen: nextProps.defaultOpen
+            })
+        }
+    */
+
+  componentWillUpdate() {
+    console.log("---", "will update");
+  }
+
+  render() {
+    const { article, isOpen, onButtonClick } = this.props;
+    const style = { width: "50%" };
+    const body = isOpen && (
+      <section className="card-text">{article.text}</section>
+    );
+    return (
+      <div
+        onClick={this.incrementCounter}
+        className="card mx-auto"
+        style={style}
+      >
+        <div className="card-header">
+          <h2>{article.title}</h2>
+          <span> {this.state.count}</span>
+          <button
+            onClick={onButtonClick}
+            className="btn btn-primary btn-lg float-right"
+          >
+            {isOpen ? "close" : "open"}
+          </button>
+        </div>
+        <div className="card-body">
+          <h6 className="card-subtitle text-muted">
+            creation date: {new Date(article.date).toDateString()}
+          </h6>
+          {body}
+        </div>
+      </div>
+    );
+  }
+
+  incrementCounter = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
+}
+
+export default Article;
